cbe1a844a8dc3b4ffecc093f148cfb73bc81d96c
Ben Brady <benbradybusiness@gmail.com>
2022-12-24 16:35:00 +0000

    Changed from register to manual_register

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from modules import cispr, config, commands
+from modules import students, cispr, config, commands
 from modules.discord import TaskCog
 import logging
 import discord
@@ -27,9 +27,9 @@ bot.slash_command(
     description="Submit an attendance code link",
 )(commands.link)
 bot.slash_command(
-    name="register",
-    description="Register for automatic attendance",
-)(commands.register)
+    name="manual_register",
+    description="Manually register for automatic attendance",
+)(commands.manual_register)
 bot.slash_command(
     name="unregister",
     description="Unregister for automatic attendance",
--- modules/commands/__init__.py
+++ modules/commands/__init__.py
@@ -1,5 +1,5 @@
 from .unregister import unregister_command as unregister
-from .register import register_command as register
+from .manual_register import manual_register_command as manual_register
 from .link import link_command as link
 from .qr import qr_command as qr
 from .update_channel import update_channel_command as update_channel
