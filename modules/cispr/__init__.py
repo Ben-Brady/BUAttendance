8cfc66cb6e48949a07b0f8fecabac4d2984181c5
Ben Brady <benbradybusiness@gmail.com>
2022-12-24 02:43:18 +0000

    Added check_token

--- modules/cispr/__init__.py
+++ modules/cispr/__init__.py
@@ -1,3 +1,3 @@
-from .session import get_global_token, set_global_token, refresh_token, CisprSessionExpiredError
+from .session import get_global_token, set_global_token, check_token, refresh_token, CisprSessionExpiredError
 from ._code import SessionCreationError, AttendanceCode
 from .register import register_attendance
